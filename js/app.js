9825fcfe35067bbb1f8180c3405766ed0ecff7e8
AmirhosseinG10 <a10.work.h@gmail.com>
2025-02-07 14:26:19 +0330

    Version 2.0

--- js/app.js
+++ js/app.js
@@ -6,21 +6,28 @@ const total_score = 63;
 
 let temp;
 for (let i = 1; i < scores.length; i++) {
-    for (let j = 0; j < scores.length - i; j++) {
-        if (Number(scores[j].innerHTML) < Number(scores[j + 1].innerHTML)) {
-            temp = scores[j + 1].innerHTML;
-            scores[j + 1].innerHTML = scores[j].innerHTML;
-            scores[j].innerHTML = temp;
-            temp = names[j + 1].innerHTML;
-            names[j + 1].innerHTML = names[j].innerHTML;
-            names[j].innerHTML = temp;
-            temp = group[j + 1].innerHTML;
-            group[j + 1].innerHTML = group[j].innerHTML;
-            group[j].innerHTML = temp;
-        }
-    }
+	for (let j = 0; j < scores.length - i; j++) {
+		if (Number(scores[j].innerHTML) < Number(scores[j + 1].innerHTML)) {
+			temp = scores[j + 1].innerHTML;
+			scores[j + 1].innerHTML = scores[j].innerHTML;
+			scores[j].innerHTML = temp;
+			temp = names[j + 1].innerHTML;
+			names[j + 1].innerHTML = names[j].innerHTML;
+			names[j].innerHTML = temp;
+			temp = group[j + 1].innerHTML;
+			group[j + 1].innerHTML = group[j].innerHTML;
+			group[j].innerHTML = temp;
+		}
+	}
 }
 
 for (let k = 0; k < names.length; k++) {
-    percent[k].innerHTML = parseInt(Number(scores[k].innerHTML) / total_score * 100) + "%";
+	percent[k].innerHTML =
+		parseInt((Number(scores[k].innerHTML) / total_score) * 100) + "%";
 }
+
+$(function () {
+	$(".fa-bars").click(function () {
+		$(".submenu").slideToggle(500);
+	});
+});
